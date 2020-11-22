51c58f352cb3a93062034540bd1b9752a3f2581e
KlimAsusKsu <turiev.klim@gmail.com>
2020-11-22 14:46:59 +0300

    homework

---
+++ src/homework1.py
@@ -0,0 +1,140 @@
+import re
+import glob
+import unittest
+import itertools
+import random
+
+#1 line: Output
+
+print ('Hello, world!')
+
+#2 lines: Input, assignment
+
+name = input('What is your name?\n')
+print ('Hi, %s.' % name)
+
+#3 lines: For loop, built-in enumerate function, new style formatting
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+
+#4 lines: Fibonacci, tuple assignment
+
+parents, babies = (1, 1)
+while babies < 100:
+    print ('This generation has {0} babies'.format(babies))
+    parents, babies = (babies, parents + babies)
+
+#5 lines: Functions
+
+def greet(name):
+    print ('Hello', name)
+
+greet('Jack')
+greet('Jill')
+greet('Bob')
+
+#6 lines: Import, regular expressions
+
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print (test_string, 'is a valid US local phone number')
+    else:
+        print (test_string, 'rejected')
+
+#7 lines: Dictionaries, generator expressions
+
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print ('I owe the grocer $%.2f' % grocery_bill)
+
+#9 lines: Opening files
+
+# indent your Python code to put into an email
+# glob supports Unix style pathname extensions
+python_files = glob.glob('*.py')
+for file_name in sorted(python_files):
+    print ('    ------' + file_name)
+
+    with open(file_name) as f:
+        for line in f:
+            print ('    ' + line.rstrip())
+
+    print()
+
+#12 lines: Classes
+
+class BankAccount(object):
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+    def deposit(self, amount):
+        self.balance += amount
+    def withdraw(self, amount):
+        self.balance -= amount
+    def overdrawn(self):
+        return self.balance < 0
+my_account = BankAccount(15)
+my_account.withdraw(50)
+print (my_account.balance, my_account.overdrawn())
+
+#18 lines: 8-Queens Problem (recursion)
+
+BOARD_SIZE = 8
+
+def under_attack(col, queens):
+    left = right = col
+
+    for r, c in reversed(queens):
+        left, right = left - 1, right + 1
+
+        if c in (left, col, right):
+            return True
+    return False
+
+def solve(n):
+    if n == 0:
+        return [[]]
+
+    smaller_solutions = solve(n - 1)
+
+    return [solution+[(n,i+1)]
+        for i in range(BOARD_SIZE)
+            for solution in smaller_solutions
+                if not under_attack(i+1, solution)]
+for answer in solve(BOARD_SIZE):
+    print (answer)
+
+#33 lines: "Guess the Number" Game (edited) from http://inventwithpython.com
+
+guesses_made = 0
+
+name = input('Hello! What is your name?\n')
+
+number = random.randint(1, 20)
+print ('Well, {0}, I am thinking of a number between 1 and 20.'.format(name))
+
+while guesses_made < 6:
+
+    guess = int(input('Take a guess: '))
+
+    guesses_made += 1
+
+    if guess < number:
+        print ('Your guess is too low.')
+
+    if guess > number:
+        print ('Your guess is too high.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    print ('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
+else:
+    print ('Nope. The number I was thinking of was {0}'.format(number))
+print('hello world')
